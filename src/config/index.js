92664f7f10ae052971cdaa5b5a4173d52d70d1bd
gongjian <225763904@qq.com>
2022-03-17 15:27:56 +0800

    DOClever环境搭建及调试

--- src/config/index.js
+++ src/config/index.js
@@ -6,15 +6,15 @@ const env = import.meta.env.MODE || "prod";
 const EnvConfig = {
   development:{
     baseApi:"/",
-    mockApi:"http://www.baidu.com"
+    mockApi:"http://192.168.182.128:20080/mock/6232dfc65f93ec000c0ded64"
   },
   test:{
     baseApi:"/",
-    mockApi:"http://www.baidu.com"
+    mockApi:"http://192.168.182.128:20080/mock/6232dfc65f93ec000c0ded64"
   },
   prod:{
     baseApi:"/",
-    mockApi:"http://www.baidu.com"
+    mockApi:"http://192.168.182.128:20080/mock/6232dfc65f93ec000c0ded64"
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
 import router from "./router/index"
 import ElementPlus from 'element-plus'
+import axios from "axios"
+import config from "./config/index"
 import 'element-plus/dist/index.css'
 
 console.log("获取环境变量",import.meta.env.MODE)
-
+console.log(config)
+axios.get(config.mockApi+'/login').then((response)=>{
+  console.log(response)
+})
 
 import App from './App.vue'
 
