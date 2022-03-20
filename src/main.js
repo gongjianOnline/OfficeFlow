033494894e5fbfadfb1d7e4443d6e167246d9930
gongjian <225763904@qq.com>
2022-03-20 16:12:07 +0800

    使用elementPlus图标的配置

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import router from "./router/index"
 import ElementPlus from 'element-plus'
+import * as ElIcons from '@element-plus/icons'
 import request from "./utils/request"
 import storage from './utils/storage'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 
 const app = createApp(App)
+//使用elementPlus图标
+for(const name in ElIcons){
+  app.component(name,ElIcons[name])
+}
 
 app.config.globalProperties.$request = request
 app.config.globalProperties.$storage = storage
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,24 @@
 <template>
-  <div> 
-    Login page
+  <div class="login-wrapper"> 
+    <div class="modal">
+      <el-form>
+        <div class="title">火星</div>
+        <el-form-item class="fromItem">
+          <el-input type="text" 
+            placeholder="请输入用户名"
+            prefix-icon="Avatar"></el-input>
+        </el-form-item>
+        <el-form-item class="fromItem">
+          <el-input type="password" 
+            placeholder="请输入密码" 
+            show-password
+            prefix-icon="Lock"></el-input>
+        </el-form-item>
+        <el-form-item class="fromItem">
+          <el-button type="primary" class="btn-login">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -24,6 +42,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .login-wrapper{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #f9fcff;
+    width: 100%;
+    height: 100vh;
+    .modal{
+      width: 500px;
+      padding: 50px;
+      background: #fff;
+      border-radius: 4px;
+      box-shadow: 0px 0px 10px 3px #c7c9cb4d;
+      .title{
+        font-size: 50px;
+        line-height: 1.5;
+        text-align: center;
+        margin-bottom: 30px;
+      }
+      .btn-login{
+        width: 100%;
+      }
+    }
+  }
 </style>
